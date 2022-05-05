3dbf3e8ae9d22ee6df95935e98b471d2b20cad0c
JunaMeinhold <46632782+JunaMeinhold@users.noreply.github.com>
2022-05-05 09:20:36 +0200

    Added files for role plugin

---
+++ GodOfUwU.Roles/Plugin.cs
@@ -0,0 +1,30 @@
+﻿namespace GodOfUwU.Roles
+{
+    using Discord.WebSocket;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+
+    public class RolePlugin : Plugin
+    {
+        public override Task SetupServices(ServiceCollection services)
+        {
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Load(IServiceProvider serviceProvider)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task Initialize(DiscordSocketClient client, IServiceProvider services)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override Task Uninitialize()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ GodOfUwU.Roles/Services/RoleService.cs
@@ -0,0 +1,12 @@
+﻿namespace GodOfUwU.Roles.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class RoleService
+    {
+    }
+}
